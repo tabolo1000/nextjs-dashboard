482ca3ec5fa32b3b896e7560436b0ee97c1dff90
tabolo1000 <tabola1000@gamil.com>
2024-09-18 14:42:27 +0300

    Chapter 6 Setting Up Database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inria_Sans, Lusitana } from 'next/font/google'
+
+
+
+export const inriaSans = Inria_Sans({
+    subsets: ['latin'],
+    weight: ["400", "700"],
+    style: "italic"
+})
+
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+})
